6754f95f5f129d843e581400cae83abefc078da0
Marion <mrobert148@gmail.com>
2023-02-25 10:45:29 +0100

    create the initial snake

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from turtle import Screen, Turtle
 
+# set tup the screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+
+# create the snake
+positions = [(0, 0), (-20, 0), (-40, 0)]
+snake = []
+
+for position in positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
+    snake.append(new_segment)
 
 
 
