4aa9ed7b1e6be1e5749744e0041288c619ee7ffe
Marion <mrobert148@gmail.com>
2023-02-25 11:58:55 +0100

    controle snake with keypress

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 # Turn turtle animation on/off and set delay for update drawings.
 screen.tracer(0)
-game_on = True
 
 # create the snake
 snake = Snake()
-
 screen.update()
+screen.listen()
+screen.onkey(fun=snake.go_up, key="Up")
+screen.onkey(fun=snake.go_down, key="Down")
+screen.onkey(fun=snake.go_right, key="Right")
+screen.onkey(fun=snake.go_left, key="Left")
 
+game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
--- snake.py
+++ snake.py
@@ -3,6 +3,7 @@ from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 
+
 class Snake:
     def __init__(self):
         self.segments = []
@@ -22,3 +23,15 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(MOVE_DISTANCE)
+
+    def go_right(self):
+        self.segments[0].setheading(0)
+
+    def go_left(self):
+        self.segments[0].setheading(180)
+
+    def go_down(self):
+        self.segments[0].setheading(270)
+
+    def go_up(self):
+        self.segments[0].setheading(90)
