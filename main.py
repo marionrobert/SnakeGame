7636565517b4960c81f4aa1491e54ac99a399424
Marion Robert <107509668+marionrobert@users.noreply.github.com>
2024-01-08 18:22:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,58 @@
-from turtle import Screen, Turtle
-from paddle import Paddle
-from ball import Ball
-from scoreboard import Scoreboard
+from turtle import Screen
 import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
-winning_score = 6
-
-# set up the playground
+# set tup the screen
 screen = Screen()
-screen.setup(width=900, height=600)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Pong Game")
+screen.title("Snake Game")
+
+
+# Turn turtle animation on/off and set delay for update drawings.
 screen.tracer(0)
 
-line = Turtle()
-line.hideturtle()
-line.pencolor("white")
-line.pensize(3)
-line.penup()
-line.goto(0, 295)
-line.setheading(270)
-for n in range(15):
-    line.pendown()
-    line.forward(20)
-    line.penup()
-    line.forward(20)
-
-# create players --> player_right and player_left
-player_right = Paddle((410, 0))
-player_left = Paddle((-410, 0))
-ball = Ball()
+# create the snake, food and scoreboard
+snake = Snake()
+food = Food()
 scoreboard = Scoreboard()
 screen.update()
 
+# controle snake with a keypress
 screen.listen()
-screen.onkeypress(fun=player_right.move_up, key="Up")
-screen.onkeypress(fun=player_right.move_down, key="Down")
-screen.onkeypress(fun=player_left .move_up, key="z")
-screen.onkeypress(fun=player_left .move_down, key="q")
+screen.onkey(fun=snake.go_up, key="Up")
+screen.onkey(fun=snake.go_down, key="Down")
+screen.onkey(fun=snake.go_right, key="Right")
+screen.onkey(fun=snake.go_left, key="Left")
 
 game_on = True
 while game_on:
-    time.sleep(ball.move_speed)
     screen.update()
-    ball.move()
+    time.sleep(0.1)
+    # move the snake
+    snake.move()
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.grow()
+        scoreboard.increase_score()
 
-    # detect collision with the wall
-    if ball.ycor() >= 280 or ball.ycor() <= -280:
-        ball.bounce_y()
+    # detect collision with wall
+    if snake.head.xcor() > 288 \
+            or snake.head.xcor() < -288 \
+            or snake.head.ycor() > 288 \
+            or snake.head.ycor() < -288:
+        scoreboard.reset()
+        snake.reset()
 
-    # detect collision with paddles
-    if ball.distance(player_right) < 60 and ball.xcor() > 380\
-            or ball.distance(player_left ) < 60 and ball.xcor() < -380:
-        ball.bounce_x()
+    # detect collision with own tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 5:
+            scoreboard.reset()
+            snake.reset()
 
-    # detect if paddle misses the ball
-    if ball.xcor() > 435:
-        scoreboard.increase_left_score()
-        ball.go_center()
 
-    if ball.xcor() < -435:
-        scoreboard.increase_right_score()
-        ball.go_center()
-
-    # detect game over
-    if scoreboard.player_left_score == winning_score or scoreboard.player_right_score == winning_score:
-        scoreboard.game_over(winning_score)
-        game_on = False
 
 screen.exitonclick()
